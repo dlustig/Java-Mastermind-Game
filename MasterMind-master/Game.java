8c4aa4d29a974a7ec59c55f5812ce311375a6580
dlustig <dllustig@gmail.com>
2014-04-08 22:45:09 -0400

    initial commit

---
+++ MasterMind-master/Game.java
@@ -0,0 +1,92 @@
+import java.util.*;
+import java.io.Console;
+public class Game{
+	public static void main(String args[]){
+                Code CorrectCode = new Code();  //instantiat code data structure for codemaker code
+                String stringcode = new String(); //temporary string to be set into code object
+                int alphsize;				//size of the alphabet
+                Scanner in = new Scanner(System.in);
+
+
+		//gets the information from the codemaker
+                System.out.println("Enter the size of the alphabet: ");
+                alphsize = in.nextInt();
+                while(!CorrectCode.setAlf(alphsize)){
+                    try {alphsize = in.nextInt();}
+
+                    catch(Exception e){
+                    	in.nextLine();
+                    	continue;
+                    }
+
+                }
+
+                System.out.println("Enter the correct code: ");
+				Console console = System.console();
+				stringcode = new String(console.readPassword()).toUpperCase();
+                //stringcode = in.next();
+                while(!CorrectCode.setCode(stringcode)){
+					stringcode = new String(console.readPassword()).toUpperCase();
+                }
+
+                System.out.println("Enter the number of rounds: ");
+                int numRounds = 0;
+                while(numRounds <= 0)
+                {
+                	try{numRounds = in.nextInt();}
+
+                	catch(Exception q){
+                		in.nextLine();
+                		continue;
+
+                	}
+                }
+
+                ActionList buttoncheck = new ActionList();
+
+		//uses the information enetered by the codemaker to create the game gui
+                MasterGUI gameBoard = new MasterGUI(numRounds, stringcode, buttoncheck);
+
+		boolean won = false;
+                int numRight;
+                int numIn;
+		int onRound=0;
+		Code GuessCode = new Code();
+		System.out.println("Start guessing!");
+
+		//The game Looop
+		while(won==false && onRound<numRounds)
+		{
+
+                        if(buttoncheck.buttonpushed){
+
+                            stringcode = gameBoard.getGuess();
+
+                            //only accept the guess if it is valid
+                            if(GuessCode.setCode(stringcode)){
+                                 stringcode = gameBoard.getGuess();
+                            	won=CorrectCode.won(GuessCode);
+                            	numRight = CorrectCode.corrects(GuessCode);
+                            	numIn = CorrectCode.ins(GuessCode);
+                           		gameBoard.submitFeedback(numRight, numIn);
+                            	onRound++;
+							}
+                            buttoncheck.buttonback();
+                        }
+
+
+		}
+		//games states for game over
+		if(won==true)
+		{
+			System.out.println("Codebreaker wins!");
+			gameBoard.gameOver();
+		} else {
+			System.out.println("Codemaker wins!");
+			gameBoard.gameOver();
+		}
+	}
+
+
+}
+
---
+++ MasterMind-master/MasterGUI.java
@@ -0,0 +1,91 @@
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;//System.currentTimeMillis();
+import java.awt.geom.*;
+import java.awt.event.*;
+
+public class MasterGUI extends JFrame
+{
+	//fonts
+	final static Font TEXT_FONT = new Font("arial",Font.BOLD,16);
+	final static Font MESSAGE_FONT = new Font("arial",Font.BOLD,12);
+
+	private JTextField guessBox = new JTextField(5);
+	private JButton submit = new JButton("Guess!");
+	private MasterMindBoard graphic;
+
+	//Constructor makes a JFrame, adds the guess menu, and the board
+	//needs the number of guesses for this game and an object to register button clicks
+	public MasterGUI(int numGuesses, String correctGuess, ActionListener listener)
+	{
+		super("MasterMind");
+
+		//layouts for the window and the menu-panel
+		BorderLayout org = new BorderLayout();
+		FlowLayout org2 = new FlowLayout();
+
+
+		//sets up the top menu
+		JPanel menu = new JPanel();
+		menu.setLayout(org2);
+		guessBox.setSize(new Dimension(200,50));
+		guessBox.setFont(TEXT_FONT);
+		submit.setSize(new Dimension(200,50));
+		submit.setFont(TEXT_FONT);
+		menu.add(guessBox,BorderLayout.NORTH);
+		menu.add(submit,BorderLayout.SOUTH);
+		submit.addActionListener(listener);
+		submit.setMnemonic(KeyEvent.VK_ENTER);//The constant value for the carriage return (enter)
+
+		graphic = new MasterMindBoard(numGuesses, correctGuess);
+
+		//sets the values for the window
+		setLayout(org);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setResizable(false);
+		setSize(400,650);
+		add(menu,BorderLayout.NORTH);
+		add(graphic);
+		setVisible(true);
+	}
+
+	public void gameOver()
+	{
+		submit.setEnabled(false);
+		guessBox.setEnabled(false);
+		graphic.gameOver();
+		repaint();
+	}
+
+	//returns what is currently in the textbox
+	public String getGuess()
+	{
+		return guessBox.getText().toUpperCase();
+	}
+
+	//registers the guess with the MasterMindBoard and resets the textbox
+	public void submitFeedback(int numRight, int numClose)
+	{
+		graphic.registerGuess(guessBox.getText().toUpperCase(),numRight,numClose);
+		guessBox.setText("");
+		guessBox.requestFocus();
+		repaint();
+	}
+
+	//used for demoing the board
+	/*public static void main(String[] args)
+	{
+		int numGuess = 30000;
+		MasterGUI obj = new MasterGUI(numGuess,"ABCD",null);
+		for(int y = 0; y < numGuess; y++)
+		{
+			for(int x = 0; x<30000;x++)
+			{
+				System.out.println(".");
+			}
+			obj.submitFeedback(1,2);
+			//obj.repaint();
+		}
+		obj.gameOver();
+	}*/
+}
